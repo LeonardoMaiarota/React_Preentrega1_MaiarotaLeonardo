8fa42ca906156446f8f09c51d4213b8954a575b9
Leonardo Maiarota <lmaiarota@gmail.com>
2024-07-11 18:46:34 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { NavBar } from "./components/NavBar";
+import { ItemListContainer } from "./components/ItemListContainer";
+
+function App() {
+  return (
+    <>
+    <NavBar />
+    <ItemListContainer greeting ="La Mejor Tienda de Tecnología"/>
+    </>
+  );
+}
+
+export default App
